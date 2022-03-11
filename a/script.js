bd643230633c0c8b696706e53fdaf21fedf5114c
Pankaj <pankaj@gmail.com>
2022-03-11 13:29:26 +0530

    changes

--- a/script.js
+++ a/script.js
@@ -1,7 +1,8 @@
 function extractSubstr() {
     var text = document.getElementById("text").value;
-    var startIdx = document.getElementById("startIdx").value;
-    var endIdx = document.getElementById("endIdx").value;
+    var startIdx = parseInt(document.getElementById("startIdx").value);
+    var endIdx = parseInt(document.getElementById("endIdx").value);
+    console.log(text);
 
     if(text.length == 0)
     {
@@ -10,22 +11,22 @@ function extractSubstr() {
     }
         
 
-    if(startIdx < 0 || startIdx >= text.length || startIdx.length == 0)
+    if(startIdx < 0 || startIdx >= text.length)
     {
         alert("Enter Correct Start Index. Start Index Out Of Bounds");
         return;
     }
         
 
-    if(endIdx < 0 || endIdx >= text.length || endIdx < startIdx || endIdx.length == 0)
+    if(endIdx < 0 || endIdx > text.length || endIdx < startIdx)
     {
         alert("Enter Correct End Index. End Index Out Of Bounds");
+        console.log(endIdx);
         return;
     }
         
 
     var substr = text.substring(startIdx, endIdx);
-
     document.getElementById("result").innerHTML = "The String Extracted From " + startIdx
                                                     + " and " + endIdx + " indices " + 
                                                     " of the string '" + text + "' is '" +
